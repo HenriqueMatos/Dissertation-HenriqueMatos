7909f5c524d93288b975ec8b8dbed629521ae601
HenriqueMatos <matoshenrique1999@gmail.com>
2022-09-07 11:41:29 +0100

    update

--- EdgeServerManager/EdgeServer.py
+++ EdgeServerManager/EdgeServer.py
@@ -1,4 +1,5 @@
 import codecs
+from enum import Flag
 import json
 import re
 from datetime import datetime
@@ -21,8 +22,9 @@ import os
 import redis
 import pickle
 # RegistrationForm,
-from forms import LoginForm
+# from forms import LoginForm
 from keycloak_utils import get_oidc2, get_oidc, get_token, check_token, verifyToken
+# from torchreid.models.nasnet import BranchSeparables
 
 
 SERVER_URL = "http://localhost:8080/auth/"
@@ -32,6 +34,7 @@ CLIENT_SECRET = "deCGEfmNbxFkC5z32UnwxtyQThTx4Evy"
 
 r = redis.Redis(host="localhost", port="6379")
 
+global client
 global DataServer
 
 if r.exists("data"):
@@ -108,7 +111,8 @@ KEYS_line_intersection_zone = {
     'end_point':  ([], "list"),
     'zone_direction_1or2': 1,
     'name_zone_before': 'outside',
-    'name_zone_after': 'inside'
+    'name_zone_after': 'inside',
+    'id_association': {}
 }
 KEYS_zone = {
     'name_inside_zone': 'pátio escola de engenharia',
@@ -145,6 +149,8 @@ def configPoints():
                 imageData = DataServer[index]["frame"]
                 data = DataServer[index]["config"][config_name_before][config_name]
                 if config_name == "line_intersection_zone":
+                    # for index in range(len(data)):
+                    #     del data[index]['id_association']
                     keys = KEYS_line_intersection_zone
                 elif config_name == "zone":
                     keys = KEYS_zone
@@ -215,16 +221,113 @@ def Setconfig():
 
 @app.route('/id_association', methods=['GET'])
 def id_association():
+    ID_associations = {}
+    ID_associationTuple = []
     DataObject = []
+
     for value in DataServer:
-        DataObject.append({
+        NoAssociationIndexes = []
+        for index, eachLine in enumerate(value["config"]["input"]["line_intersection_zone"]):
+            # print(eachLine)
+            if eachLine.__contains__('id_association'):
+                if eachLine['id_association']:
+                    # HAS ASSOCIATION
+                    ID_associations[eachLine['name']
+                                    ] = {"name": eachLine['id_association']['name'],
+                                         "preferred_username": value["preferred_username"]}
+                else:
+                    NoAssociationIndexes.append(index)
+            else:
+                NoAssociationIndexes.append(index)
 
-            "frame":  base64.b64encode(base64.decodebytes(value["frame"].encode('utf-8'))).decode('utf-8'),
-            "preferred_username": value["preferred_username"],
-            "line_intersection_zone": value["config"]["input"]["line_intersection_zone"]
-        })
+        if len(NoAssociationIndexes) > 0:
+            DataObject.append({
+                "frame":  base64.b64encode(base64.decodebytes(value["frame"].encode('utf-8'))).decode('utf-8'),
+                "preferred_username": value["preferred_username"],
+                "line_intersection_zone": [value["config"]["input"]["line_intersection_zone"][x] for x in NoAssociationIndexes]
+            })
+    Associations = []
+    for key, value in ID_associations.items():
+        if key in [value2["name"] for value2 in ID_associations.values()]:
+            notisInTuple = False
 
-    return render_template('id_association.html',DataObject=DataObject)
+            for eachTuple in Associations:
+                if key in eachTuple:
+                    notisInTuple = True
+                    break
+
+            if notisInTuple == False:
+                Associations.append([key, value["preferred_username"], value["name"],
+                                     ID_associations[value["name"]]["preferred_username"]])
+
+    print("FFFFFFFFFF", Associations)
+
+    return render_template('id_association.html', DataObject=DataObject, Associations=Associations)
+
+
+@app.route('/id_association/get_association', methods=['GET'])
+def getAssociations():
+    ID_associations = {}
+
+    for value in DataServer:
+        for index, eachLine in enumerate(value["config"]["input"]["line_intersection_zone"]):
+            # print(eachLine)
+            if eachLine.__contains__('id_association'):
+                if eachLine['id_association']:
+                    # HAS ASSOCIATION
+                    ID_associations[eachLine['name']
+                                    ] = {"name": eachLine['id_association']['name'],
+                                         "preferred_username": value["preferred_username"]}
+    Associations = []
+    for key, value in ID_associations.items():
+        if key in [value2["name"] for value2 in ID_associations.values()]:
+            notisInTuple = False
+
+            for eachTuple in Associations:
+                if key in eachTuple:
+                    notisInTuple = True
+                    break
+
+            if notisInTuple == False:
+                Associations.append([key, value["preferred_username"], value["name"],
+                                     ID_associations[value["name"]]["preferred_username"]])
+
+    return jsonify(Associations=Associations)
+
+
+@app.route('/id_association/remove_association', methods=['POST'])
+def RemoveAssociation():
+    if request.method == "POST":
+        print(request.form)
+        data = json.loads(request.form["data"])
+        # data = json.loads(request.form["data"])
+        print(data)
+        for eachData in data:
+            print(eachData["Name"])
+            print(eachData["ConfigName"])
+        Names = [value2["Name"] for value2 in data]
+        ConfigNames = [value2["ConfigName"] for value2 in data]
+
+        for index in range(len(DataServer)):
+            if DataServer[index]["preferred_username"] in Names:
+                indexToRemove = Names.index(
+                    DataServer[index]["preferred_username"])
+
+                for index_line_intersection_Config in range(len(DataServer[index]["config"]["input"]["line_intersection_zone"])):
+                    if DataServer[index]["config"]["input"]["line_intersection_zone"][index_line_intersection_Config]["name"] == ConfigNames[indexToRemove]:
+                        DataServer[index]["config"]["input"]["line_intersection_zone"][index_line_intersection_Config]["id_association"] = {
+                        }
+                        # SEND TO TRACKING SYSTEMS
+                        sendData = {}
+                        sendData["type"] = "update"
+                        sendData["config"] = DataServer[index]["config"]
+                        client.publish("edge_config/"+DataServer[index]["preferred_username"],
+                               json.dumps(sendData))
+
+                Names.pop(indexToRemove)
+                ConfigNames.pop(indexToRemove)
+
+        return "OK"
 
 
 def web():
